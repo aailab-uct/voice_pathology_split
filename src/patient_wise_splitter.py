ebc69d2d21cfc9c941f1f8c3d71f8f31acd13f50
jirsat <45206873+jirsat@users.noreply.github.com>
2024-04-05 14:56:43 +0200

    update reproducibility split for patients_wise_datasets

--- src/patient_wise_splitter.py
+++ src/patient_wise_splitter.py
@@ -40,9 +40,19 @@ for spectrogram_path in tqdm(sorted(list(path_to_dataset.glob("*.*")))):
     src =spectrogram_path.read_bytes()
     dest.joinpath(spectrogram_path.name).write_bytes(src)
 
-# randomly delete 4 files to get 1985 samples in test set
-test_set_paths = list(dataset_path.joinpath("test").glob("**/*"))
-samples_to_delete = rnd.sample(test_set_paths, 4)
+# # randomly delete 4 files to get 1985 samples in test set
+# test_set_paths = list(dataset_path.joinpath("test").glob("**/*"))
+# samples_to_delete = rnd.sample(test_set_paths, 4)
+
+## As the glob is not deterministic and we forgot about it,
+## we will remove the files that we know we should remove
+samples_to_delete = [
+    dataset_path.joinpath("test", "unhealthy", "svdadult1887_unhealthy_50000_00008.png"),
+    dataset_path.joinpath("test", "unhealthy", "svdadult1625_unhealthy_50000_00000.png"),
+    dataset_path.joinpath("test", "unhealthy", "svdadult1119_unhealthy_50000_00002.png"),
+    dataset_path.joinpath("test", "healthy", "svdadult1543_healthy_50000_00005.png")
+]
 
 for to_delete in samples_to_delete:
     to_delete.unlink()
+
