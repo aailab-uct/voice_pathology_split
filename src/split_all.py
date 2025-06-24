553c5535fa2ecc6f7eb0ac49b6d62439a45181a2
steinbachjakub <84331693+steinbachjakub@users.noreply.github.com>
2025-06-24 16:51:15 +0900

    Rearanged spectrograms, modified code to simplify the methodology of splits, added model validation in form of confusion matrix

--- src/split_all.py
+++ src/split_all.py
@@ -3,11 +3,11 @@ This script runs all the split scripts in the src folder.
 """
 import importlib
 split_scripts = [
-    # 'random_splitter',
-    # 'random_splitter_voiced',
-    # 'each_patient_random_segment_splitter',
-    # 'each_patient_random_segment_splitter_voiced',
-    # 'recording_wise_splitter',
+    'random_splitter',
+    'random_splitter_voiced',
+    'each_patient_random_segment_splitter',
+    'each_patient_random_segment_splitter_voiced',
+    'recording_wise_splitter',
     'patient_wise_splitter_voiced',
     'each_patient_same_segment_splitter',
     'each_patient_same_segment_splitter_voiced',
