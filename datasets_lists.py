ebf1b93e5db85126b627bb14c6560b7cd2411319
xr72 <Jan.Vrba@vscht.cz>
2025-07-11 15:13:51 +0900

    datasets_lists.py printing removed

--- datasets_lists.py
+++ datasets_lists.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
         print(split_strategy)
         files_list = [[str(Path(*image_path.parts[2:]))] for image_path in split_strategy.glob("**/*.png")]
         print(len(files_list))
-        print(files_list)
         with open(split_strategy.name + ".csv", "w") as f:
             writer = csv.writer(f, delimiter=',', quotechar="'", lineterminator='\n')
             writer.writerows(files_list)
