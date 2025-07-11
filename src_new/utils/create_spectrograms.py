9a20b7a1931f7ae3cf8e1a9bafd244ab9f3c8015
steinbachjakub <84331693+steinbachjakub@users.noreply.github.com>
2025-07-11 13:40:17 +0900

    New scripts for spectrogram generation from SVD data.

--- src_new/utils/create_spectrograms.py
+++ src_new/utils/create_spectrograms.py
@@ -6,19 +6,36 @@ import pandas as pd
 from tqdm import tqdm
 import shutil
 
-from utilities.converters import wav2spectrogram, txt2wav
+from src.utils.converters import wav2spectrogram, txt2wav
 
 # SVD wav to txt
 print("Transforming files SVD wav files to txt files...")
-svd_path = Path("datasets","svd")
-svd_path_renamed = Path("datasets","svdadult_renamed")
-svd_path_renamed.mkdir(parents=True, exist_ok=True)
+USED_SVD_LIST_PATH = Path("misc", "used_recordings.csv")
+SVD_PATH = Path("svd-db")
+PATH_SPECTROGRAMS = Path("spectrograms", "svd")
+PATH_SPECTROGRAMS.mkdir(parents=True, exist_ok=True)
 scaler = MinMaxScaler((-1, 1))
 
-with open("svd_filelist.txt", "r") as f:
-  data = f.readlines()
+fft_len = 256  # length of fft window (Hamming is used by default)
+fft_overlap = 128  # overlap of ffts windows
+spectrogram_resolution = (480, 480)  # desired spectrogram resolution as tuple
+octaves = []  # specify octave filters if needed, see utilities.octave_filter_bank.py for details
+chunks = 10  # each wav file is split into multiple chunks, set the number of chunks
+sample_rate = 50000
 
-downloaded_files = [i.stem for i in svd_path.glob("*.wav")]
+def create_spectrograms():
+    data = pd.read_csv(USED_SVD_LIST_PATH)
+    print(data.head())
+    for idx, row in data.iterrows():
+        source_file_path = SVD_PATH.joinpath(f'{row["recording_id"]}-a_n.wav')
+        destination_file_path = f'svd_patient_{row["patient_id"]:0>4}_{row["state"]}_record_{row["recording_id"]}.wav'
+        if source_file_path.exists():
+            wav2spectrogram(source_file_path, destination_path_spectrogram, fft_len, fft_overlap,
+                            spectrogram_resolution, octaves=[], standard_chunk=False,
+                            resampling_freq=None)
+
+"""
+downloaded_files = [i.stem for i in SVD_PATH.glob("*.wav")]
 
 not_found = []
 
@@ -29,7 +46,7 @@ for line in tqdm(data, unit="file"):
   if not filename in downloaded_files:
     not_found.append(filename)
     continue
-  file = svd_path.joinpath(filename + ".wav")
+  file = SVD_PATH.joinpath(filename + ".wav")
 
   file_array = read(file)
   file_array_float = np.array(file_array[1], dtype="f")
@@ -72,7 +89,7 @@ for file in tqdm(voiced_path.glob("*.hea"), unit="file",
 
 # Create spectrograms of voiced
 dataset = "voiced"
-svd_path = Path("datasets", f"{dataset}_renamed")
+SVD_PATH = Path("datasets", f"{dataset}_renamed")
 destination_path_wavs = Path("datasets", f"{dataset}_renamed_wavs")
 destination_path_spectrogram = Path("datasets", f"spectrogram_{dataset}")
 destination_path_wavs.mkdir(parents=True, exist_ok=True)
@@ -85,7 +102,7 @@ octaves = []  # specify octave filters if needed, see utilities.octave_filter_ba
 chunks = 10  # each wav file is split into multiple chunks, set the number of chunks
 sample_rate = 8000
 
-for txt_file in svd_path.glob("*.*"):
+for txt_file in SVD_PATH.glob("*.*"):
   txt2wav(txt_file, destination_path_wavs, sample_rate, chunks)
 
 print("Creating spectrograms of VOICED...")
@@ -98,7 +115,7 @@ for wav_file in tqdm(destination_path_wavs.glob("*.*"),
 
 # Create spectrograms of svd
 dataset = "svdadult"
-svd_path = Path("datasets", f"{dataset}_renamed")
+SVD_PATH = Path("datasets", f"{dataset}_renamed")
 destination_path_wavs = Path("datasets", f"{dataset}_renamed_wavs")
 destination_path_spectrogram = Path("datasets", f"spectrogram")
 destination_path_wavs.mkdir(parents=True, exist_ok=True)
@@ -111,7 +128,7 @@ octaves = []  # specify octave filters if needed, see utilities.octave_filter_ba
 chunks = 10  # each wav file is split into multiple chunks, set the number of chunks
 sample_rate = 50000
 
-for txt_file in svd_path.glob("*.*"):
+for txt_file in SVD_PATH.glob("*.*"):
   txt2wav(txt_file, destination_path_wavs, sample_rate, chunks)
 
 print("Creating spectrograms of SVD...")
@@ -121,3 +138,4 @@ for wav_file in tqdm(destination_path_wavs.glob("*.*"),
     wav2spectrogram(wav_file, destination_path_spectrogram, fft_len, fft_overlap,
                                     spectrogram_resolution, octaves=[], standard_chunk=False,
                                     resampling_freq=None)
+"""
