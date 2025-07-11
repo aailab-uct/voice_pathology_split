93a852c525ca1547d176657bc32fedcf12921adb
steinbachjakub <84331693+steinbachjakub@users.noreply.github.com>
2025-07-11 13:45:15 +0900

    Renaming of src module.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # Utility functions for generating spectrograms from SVD recordings, if missing, afor preparation of datasets for each
 # scenario, and for running the classification on all datasets
-from src_new.utils.create_spectrograms import create_spectrograms
+from src.utils.create_spectrograms import create_spectrograms
 # from src import split_all, classification_runner
 
 create_spectrograms()
