f050da95de5abd93c86603d288b0b6af566aa3cc
steinbachjakub <84331693+steinbachjakub@users.noreply.github.com>
2025-07-14 18:00:13 +0900

    Added a script for conducting the experiments.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # scenario, and for running the classification on all datasets
 from src.utils.create_spectrograms import create_spectrograms_from_SVD, create_spectrograms_from_VOICED
 from src.split_all import sample_ext_validation, split_scenario_1, split_scenario_2, split_scenario_3, split_scenario_4_voiced_5_svd, split_scenario_4_svd_6
-# from src import split_all, classification_runner
+from src.classification_runner import run_experiments
 from pathlib import Path
 import pandas as pd
 
@@ -14,55 +14,59 @@ PATH_TO_SPECTROGRAMS = Path("spectrograms")
 PATH_USED_SVD_FILES = Path("misc", "used_svd_recordings.csv")
 PATH_USED_VOICED_FILES = Path("misc", "used_voiced_recordings.csv")
 
-# Generate spectrograms from SVD recordings if not existing
-if not PATH_TO_SPECTROGRAMS.joinpath("svd").exists():
-    if PATH_TO_SVD_AUDIO.exists():
+if __name__ == "__main__":
+    # Generate spectrograms from SVD recordings if not existing
+    if not PATH_TO_SPECTROGRAMS.joinpath("svd").exists():
+        if PATH_TO_SVD_AUDIO.exists():
+            print("Folder with SVD spectrograms not found, generating spectrograms from SVD audio...")
+            used_recordings = set(pd.read_csv(PATH_USED_SVD_FILES).recording_id.tolist())
+            found_recordings = set(map(lambda x: int(x.name.split("-")[0]), PATH_TO_SVD_AUDIO.glob("*.wav")))
+            missing_recordings = used_recordings - found_recordings
+            if len(missing_recordings) == 0:
+                PATH_TO_SPECTROGRAMS.joinpath("svd").mkdir(parents=True)
+                create_spectrograms_from_SVD()
+            else:
+                print("The following recordings were not found in the database:",
+                      ", ".join(map(lambda x: f"{x}-a_n.wav", missing_recordings)))
+                raise FileNotFoundError
+        else:
+            print("Folder with SVD audio files not found. Please, place all SVD audio recordings with /a:/ sound in "
+                  "neutral pitch in", PATH_TO_SVD_AUDIO)
+            raise FileNotFoundError
+
+    # Generate spectrograms from VOICED recordings if not existing
+    if not PATH_TO_SPECTROGRAMS.joinpath("voiced").exists():
         print("Folder with SVD spectrograms not found, generating spectrograms from SVD audio...")
-        used_recordings = set(pd.read_csv(PATH_USED_SVD_FILES).recording_id.tolist())
-        found_recordings = set(map(lambda x: int(x.name.split("-")[0]), PATH_TO_SVD_AUDIO.glob("*.wav")))
-        missing_recordings = used_recordings - found_recordings
-        if len(missing_recordings) == 0:
-            PATH_TO_SPECTROGRAMS.joinpath("svd").mkdir(parents=True)
-            create_spectrograms_from_SVD()
+        if PATH_TO_VOICED_AUDIO.exists():
+            print("Folder with VOICED spectrograms not found, generating spectrograms from VOICED audio...")
+            used_recordings = set(pd.read_csv(PATH_USED_VOICED_FILES).recording_id.tolist())
+            found_recordings = set(map(lambda x: int(x.stem[-3:]), PATH_TO_VOICED_AUDIO.glob("*.dat")))
+            missing_recordings = found_recordings - used_recordings
+            if len(missing_recordings) == 0:
+                PATH_TO_SPECTROGRAMS.joinpath("voiced").mkdir(parents=True)
+                create_spectrograms_from_VOICED()
+            else:
+                print("The following recordings were not found in the database:",
+                      ", ".join(map(lambda x: f"voice{x}.txt", missing_recordings)))
+                raise FileNotFoundError
         else:
-            print("The following recordings were not found in the database:",
-                  ", ".join(map(lambda x: f"{x}-a_n.wav", missing_recordings)))
+            print("Folder with VOICED audio files not found. Please, place all VOICED audio recordings in",
+                  PATH_TO_VOICED_AUDIO)
             raise FileNotFoundError
-    else:
-        print("Folder with SVD audio files not found. Please, place all SVD audio recordings with /a:/ sound in "
-              "neutral pitch in", PATH_TO_SVD_AUDIO)
-        raise FileNotFoundError
 
-# Generate spectrograms from VOICED recordings if not existing
-if not PATH_TO_SPECTROGRAMS.joinpath("voiced").exists():
-    print("Folder with SVD spectrograms not found, generating spectrograms from SVD audio...")
-    if PATH_TO_VOICED_AUDIO.exists():
-        print("Folder with VOICED spectrograms not found, generating spectrograms from VOICED audio...")
-        used_recordings = set(pd.read_csv(PATH_USED_VOICED_FILES).recording_id.tolist())
-        found_recordings = set(map(lambda x: int(x.stem[-3:]), PATH_TO_VOICED_AUDIO.glob("*.dat")))
-        missing_recordings = found_recordings - used_recordings
-        if len(missing_recordings) == 0:
-            PATH_TO_SPECTROGRAMS.joinpath("voiced").mkdir(parents=True)
-            create_spectrograms_from_VOICED()
-        else:
-            print("The following recordings were not found in the database:",
-                  ", ".join(map(lambda x: f"voice{x}.txt", missing_recordings)))
-            raise FileNotFoundError
-    else:
-        print("Folder with VOICED audio files not found. Please, place all VOICED audio recordings in",
-              PATH_TO_VOICED_AUDIO)
-        raise FileNotFoundError
+    # Randomly choose patients for "external" validation
+    ext_val_samples = sample_ext_validation()
 
-# Randomly choose patients for "external" validation
-ext_val_samples = sample_ext_validation()
+    # Prepare datasets for scenarios 1-3, 4 (VOICED), and 5 (SVD)
+    for db in ["svd", "voiced"]:
+        split_scenario_1(db, ext_val_samples)
+        split_scenario_2(db, ext_val_samples)
+        split_scenario_3(db, ext_val_samples)
+        split_scenario_4_voiced_5_svd(db, ext_val_samples)
 
-# Prepare datasets for scenarios 1-3, 4 (VOICED), and 5 (SVD)
-for db in ["svd", "voiced"]:
-    split_scenario_1(db, ext_val_samples)
-    split_scenario_2(db, ext_val_samples)
-    split_scenario_3(db, ext_val_samples)
-    split_scenario_4_voiced_5_svd(db, ext_val_samples)
+    # Prepare datasets for scenarios 4 (SVD) and 6
+    for boolean in [True, False]:
+        split_scenario_4_svd_6(ext_val_samples, exclude_duplicates=boolean)
 
-# Prepare datasets for scenarios 4 (SVD) and 6
-for boolean in [True, False]:
-    split_scenario_4_svd_6(ext_val_samples, exclude_duplicates=boolean)
+    # Run the experiments
+    run_experiments()
--- src/classification_runner.py
+++ src/classification_runner.py
@@ -3,67 +3,65 @@ This script is used to test the classification performance of the YOLOv8 model
 on differently split datasets.
 """
 #pylint: disable=use-maxsplit-arg,invalid-name
-import os
 from pathlib import Path
 from ultralytics import YOLO
 import pandas as pd
 
-
 PATH_DATASETS = Path("datasets")
+BASE_DIR = Path(__file__).resolve().parent.parent
+PATH_YAML = Path("misc", "yaml_template.txt")
+MODELS = ["yolov8n-cls.pt",
+          "yolov8s-cls.pt",
+          "yolov8m-cls.pt",
+          "yolov8l-cls.pt",
+          "yolov8x-cls.pt"]
 
-if __name__ == "__main__":
-    results_table = pd.DataFrame(columns=["Approach/dataset", "Architecture", "ACC", "SPE", "SEN", "TN", "TP", "FN",
-                                          "FP"])
+EPOCHS = 300
 
-    with open("yaml_template.txt", "r") as template:
-        yaml = template.read()
+def run_experiments():
+    results_table = pd.DataFrame(columns=["scenario", "size",
+                                    "val_acc", "val_uar", "val_tp", "val_fp", "val_tn", "val_fn",
+                                    "test_acc", "test_uar", "test_tp", "test_fp", "test_tn", "test_fn"])
 
-    datasets = ['patients_wise_split_svd_no_duplicities',
-                'patients_wise_split_svd_with_duplicities',
-                'patients_wise_split_voiced',
-                'segmentation_leakage_random_segment_of_each_recording_svd',
-                'segmentation_leakage_random_segment_of_each_recording_voiced',
-                'segmentation_leakage_random_split_svd',
-                'segmentation_leakage_random_split_voiced',
-                'segmentation_leakage_recording_wise_split_svd',
-                'segmentation_leakage_same_segment_of_each_recording_svd',
-                'segmentation_leakage_same_segment_of_each_recording_voiced'
-                ]
 
-    models = ["yolov8n-cls.pt",
-              "yolov8s-cls.pt",
-              "yolov8m-cls.pt",
-              "yolov8l-cls.pt",
-              "yolov8x-cls.pt"
-              ]
 
-    epochs = 300
-    
-    for folder_name in datasets:
-        # folder_abs_path = os.path.join(ABS_PATH, folder_name)
-        for model_name in models:
-            # Load a model
-            model = YOLO(os.path.join("", "models", model_name))
+    scenarios = [x.name for x in PATH_DATASETS.glob("*")]
 
+    # Train all models for each scenario
+    for scenario in scenarios:
+        for model_name in MODELS:
+            # Define the path to the dataset for YOLO model
+            dataset_path = BASE_DIR.joinpath("datasets", scenario)
+            # Load the YOLO model
+            model = YOLO(Path("models", model_name))
             # Train the model
-            model.train(data=PATH_DATASETS.joinpath(folder_name), optimizer="SGD", epochs=epochs,
-                        name=f"{folder_name}_{model_name.split('.')[0]}_{epochs}_sgd")
+            model.train(data=dataset_path, optimizer="SGD", epochs=EPOCHS,
+                        name=f"{scenario}_{model_name.split('.')[0]}_{EPOCHS}_sgd")
             print("#"*30, "Validation", "#"*30)
-            model = YOLO(os.path.join("runs", "classify", f"{folder_name}_{model_name.split('.')[0]}_{epochs}_sgd",
-                                      "weights", "best.pt"))
 
-            with open("test.yaml", "w") as yaml_file:
-                yaml_file.write(yaml.format(path_to_data=folder_name))
+            # Define the path to the best model settings
+            best_model_path = BASE_DIR.joinpath("runs", "classify",
+                                                f"{scenario}_{model_name.split('.')[0]}_{EPOCHS}_sgd", "weights",
+                                                "best.pt")
+            model = YOLO(best_model_path)
 
-            results = model.val(data="test.yaml")  # use your custom dataset YAML
+            results_val = model.val(data=dataset_path, split="val")
+            print(results_val)
 
-            TP = results.confusion_matrix.matrix[1][1]
-            TN = results.confusion_matrix.matrix[0][0]
-            FP = results.confusion_matrix.matrix[0][1]
-            FN = results.confusion_matrix.matrix[1][0]
-            ACC = (TP + TN) / (TP + TN + FN + FP)
-            SEN = TP / (TP + FN)
-            SPE = TN / (TN + FP)
+            results_test = model.val(data=dataset_path, split="test")
+            print(results_test)
 
-            results_table.loc[len(results_table.index)] = [folder_name, model_name, ACC, SPE, SEN, TN, TP, FN, FP]
+            row = []
+            for subset, result in zip(["val", "test"], [results_val, results_test]):
+                TP = result.confusion_matrix.matrix[1][1]
+                TN = result.confusion_matrix.matrix[0][0]
+                FP = result.confusion_matrix.matrix[0][1]
+                FN = result.confusion_matrix.matrix[1][0]
+                ACC = (TP + TN) / (TP + TN + FN + FP)
+                SEN = TP / (TP + FN)
+                SPE = TN / (TN + FP)
+                UAR = (SEN + SPE) / 2
+                row.append([ACC, UAR, TP, FP, TN, FN])
+
+            results_table.loc[len(results_table.index)] = [scenario, model_name] + row
             results_table.to_csv("segmentation_leakage_results.csv")
